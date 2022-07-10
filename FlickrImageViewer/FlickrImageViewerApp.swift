fbd5e1cac3e257e9eeadfa1dc6ef828e6c0e50cc
Natalia Klymenko <natalia.klymenko@accelo.com>
2022-07-10 14:45:43 +1000

    Initial Commit

---
+++ FlickrImageViewer/FlickrImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageViewerApp.swift
+//  FlickrImageViewer
+//
+//  Created by Natalia Klymenko on 10.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
